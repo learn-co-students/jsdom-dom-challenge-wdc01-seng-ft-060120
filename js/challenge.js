1b3a8d843d80d290d4e1763871ba24c87fd37f3b
Kosi Akporji <kosiazom@gmail.com>
2020-07-15 15:24:42 -0400

    updated clickHeart, aadded LI to each ul

--- js/challenge.js
+++ js/challenge.js
@@ -1,5 +1,11 @@
 document.addEventListener('DOMContentLoaded', function(){
     let count = document.getElementById('counter');
+    let plus = document.getElementById('plus');
+    let minus = document.getElementById('minus');
+    let ul = document.querySelector('.likes');
+   
+    let heart = document.getElementById('heart');
+    
     
     function addSecond(){
         let countAsInt = parseInt(count.textContent, 10); 
@@ -15,18 +21,29 @@ document.addEventListener('DOMContentLoaded', function(){
         console.log(countAsInt);
     };
 
+    function clickHeart(){
+        let li = document.createElement("li");
+     let someLi = ul.appendChild(li);
+     let newLi =someLi.innerText = `${count.textContent} has been liked time`
+
+      console.log(count.textContent)
+    };
+
+
     setInterval(addSecond, 1000);
     // let interval = window.setInterval(addSecond, 1000);
     // console.log(interval);
 
-    let plus = document.getElementById('plus');
-    let minus = document.getElementById('minus');
 
     plus.addEventListener('click', function(e){
         addSecond();
     });
     minus.addEventListener('click', function(e){
         minusSecond();
+    });
+
+    heart.addEventListener('click',function(e){
+        clickHeart();
     });
 
 })
