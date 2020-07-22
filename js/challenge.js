ecfe4eac89466fbc559bc032af34a2f299aa34c8
Marcos Velasco <devslife7@gmail.com>
2020-07-21 23:31:03 -0400

    Done.

--- js/challenge.js
+++ js/challenge.js
@@ -1,63 +1,67 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
+    const counter = document.getElementById('counter')
+    const heartButton = document.getElementById('heart')
+    const plusButton = document.getElementById('plus')
+    const minusButton = document.getElementById('minus')
+    const pauseButton = document.getElementById('pause')
+    const likesUl = document.querySelector('ul.likes')
+    const submitButton = document.getElementById('submit')
+    const commentDiv = document.querySelector('div#list')
+    let commentInput = document.getElementById('comment-input')
 
-    const counter = document.getElementById('counter');
-    const plusButton = document.getElementById('plus');
-    const minusButton = document.getElementById('minus');
-    const heartButton = document.getElementById('heart');
-    const pauseButton = document.getElementById('pause');
-    const likesUl = document.querySelector('ul.likes');
-    const submitButton = document.getElementById('submit');
-    const commentDiv = document.querySelector('div#list');
-    let commentInput = document.getElementById('comment-input');
+    let timer = setInterval(() => {
+        counter.innerHTML++
+    }, 1000)
 
-    let timer = setInterval(() => { counter.innerHTML++ }, 1000);
-
-    plusButton.addEventListener('click', function(e){
-        counter.innerHTML++;
-    });
-    minusButton.addEventListener('click', function(e){
-        counter.innerHTML--;
-    });
-
-    heartButton.addEventListener('click', function(e) {
-        let number = counter.innerText;
-        if (document.getElementById(number) !== null){
-            let likesCounter = document.getElementById(number);
-            likesCounter.innerText = `${parseInt(likesCounter.innerText) + 1} times`;
-        } else {
-            const likesLi = document.createElement('li');
-            likesLi.innerHTML = `${number} has been liked <span id=${number}>1 time</span>`;
-            likesUl.append(likesLi);
+    // PRACTICE EVENT DELEGATION
+    document.addEventListener('click', e => {
+        const elementClicked = e.target
+        if (elementClicked.matches('#minus')) {
+            counter.innerHTML--
         }
-    });
+        if (elementClicked.matches('#plus')) {
+            counter.innerHTML++
+        }
+        if (elementClicked.matches('#heart')) {
+            const currentCount = counter.innerText
+            if (document.getElementById(currentCount) === null) {
+                const likesLi = document.createElement('li')
+                likesLi.innerHTML = `${currentCount} has been liked <span id=${currentCount}>1 time</span>`
+                likesUl.append(likesLi)
+            } else {
+                let likesCounter = document.getElementById(currentCount)
+                likesCounter.innerText = `${parseInt(likesCounter.innerText) + 1} times`
+            }
+        }
+    })
 
     pauseButton.addEventListener('click', function (e) {
-
-        if (pauseButton.innerHTML === ' pause '){
-            clearInterval(timer);
-            pauseButton.innerHTML = 'resume';
-            plusButton.disabled = true;
-            minusButton.disabled = true;
-            heartButton.disabled = true;
-            submitButton.disabled = true;
+        if (pauseButton.innerHTML === ' pause ') {
+            clearInterval(timer)
+            pauseButton.innerHTML = 'resume'
+            plusButton.disabled = true
+            minusButton.disabled = true
+            heartButton.disabled = true
+            submitButton.disabled = true
         } else {
-            pauseButton.innerHTML = ' pause ';
-            plusButton.disabled = false;
-            minusButton.disabled = false;
-            heartButton.disabled = false;
-            submitButton.disabled = false;
-            timer = setInterval(function(){ counter.innerHTML++ }, 1000);
+            pauseButton.innerHTML = ' pause '
+            plusButton.disabled = false
+            minusButton.disabled = false
+            heartButton.disabled = false
+            submitButton.disabled = false
+            timer = setInterval(function () {
+                counter.innerHTML++
+            }, 1000)
         }
     })
 
     submitButton.addEventListener('click', function (e) {
-        e.preventDefault();
-        console.log(commentInput.value);
-        
-        let newComment = document.createElement('p');
-        newComment.innerHTML = commentInput.value;
-        commentDiv.append(newComment);
+        e.preventDefault()
 
-        commentInput.value = "";
+        let newComment = document.createElement('p')
+        newComment.innerHTML = commentInput.value
+        commentDiv.append(newComment)
+
+        commentInput.value = ''
     })
-});
+})
